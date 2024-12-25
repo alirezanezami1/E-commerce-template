cc9321c7fb9805ffe5abea2db77aa256645b71b5
alireza nezami <nezamialireza1022@gmail.com>
2024-12-25 23:48:21 +0330

    add register section

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,8 +1,22 @@
+let authForm = document.querySelector('.auth-form')
+let signIn = document.querySelector('.sign-in')
+let signUp = document.querySelector('.sign-up')
+
+signIn.addEventListener('click' , () => {
+    authForm.classList.remove('signInEnter')
+})
+
+signUp.addEventListener('click' , () => {
+    authForm.classList.add('signInEnter')
+})
+
 const usernuminpt = document.querySelector(".usernum_inpt")
-const regbtn = document.querySelector(".reg_btn")
+const regbtn = document.querySelectorAll(".reg_btn")
 // localStorage.removeItem("num")
 
-regbtn.addEventListener("click",sendcodeH)
+regbtn.forEach(btn => {
+    btn.addEventListener("click",sendcodeH)
+})
 
 function sendcodeH(){
     if (usernuminpt.value.length == 11 || usernuminpt.value.length == 10) {
@@ -11,18 +25,16 @@ function sendcodeH(){
         .then(resp => console.log(resp))
         .then(gocheckcodep())
     } else{
-        alert(1,"لطفا شماره موبایل خود را به درستی وارد نمایید")
+        alert("لطفا شماره موبایل خود را به درستی وارد نمایید")
         location.reload()
-        regbtn.setAttribute("href" , "#")
+        regbtn.forEach(btn => {
+            btn.setAttribute("href" , "#")
+        })
         
     }
 }
 function gocheckcodep(){
-    // localStorage.setItem("num",usernuminpt.value)
-    regbtn.setAttribute("href" , "sending-code.html?num="+Number(usernuminpt.value))
-    // location.href
+    regbtn.forEach(btn => {
+        btn.setAttribute("href" , "sending-code.html?num="+Number(usernuminpt.value))
+    })    
 }
-// function sendagaincode(e){
-//   console.log(e.target);
-  
-// }
